59226d834b9e5131eb2eb6933880f86c3b82619a
Sam Okasha <okasha86@hotmail.com>
2018-09-08 22:11:38 +0400

    Add signup and signin component styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,73 +2,93 @@ import React from 'react';
 import { connect } from 'react-redux';
 import validator from 'validator';
 
-import {startLogin} from '../actions/auth';
+import { startLogin } from '../actions/auth';
 
 export class LoginPage extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       email: '',
       password: '',
       error: ''
-    }
+    };
   }
-  onEmailChange = (e) => {
-    this.setState({email: e.target.value}, () => {
-      !validator.isEmail(this.state.email) ? this.setState({error: 'Please  enter a valid email'}) : this.setState({error: ''});
+  onEmailChange = e => {
+    this.setState({ email: e.target.value }, () => {
+      !validator.isEmail(this.state.email)
+        ? this.setState({ error: 'Please  enter a valid email' })
+        : this.setState({ error: '' });
     });
-  }
-  onPasswordChange = (e) => {
-    this.setState({password: e.target.value}, () => {
-      validator.isEmpty(this.state.password) ? this.setState({error: 'Please enter your password'}) : this.setState({error: ''});
+  };
+  onPasswordChange = e => {
+    this.setState({ password: e.target.value }, () => {
+      validator.isEmpty(this.state.password)
+        ? this.setState({ error: 'Please enter your password' })
+        : this.setState({ error: '' });
     });
-  }
-  onSubmit = async (e) => {
+  };
+  onSubmit = async e => {
     e.preventDefault();
     try {
-      const {email, password} = this.state;
+      const { email, password } = this.state;
       await this.props.startLogin(email, password);
       this.props.history.push('/');
     } catch (e) {
-      return this.setState({error: e.message})
+      return this.setState({ error: e.message });
     }
-  }
+  };
   isDisabled = () => {
-    return !this.state.error && validator.isEmail(this.state.email) && !validator.isEmpty(this.state.password)? false : true;
-  }
+    return !this.state.error &&
+      validator.isEmail(this.state.email) &&
+      !validator.isEmpty(this.state.password)
+      ? false
+      : true;
+  };
   renderError = () => {
-    if(this.state.error) {
-      return (
-        <p className="error-msg">{this.state.error}</p>
-      )
+    if (this.state.error) {
+      return <p className="error-msg alert alert-danger" role="alert">{this.state.error}</p>;
     }
-  }
+  };
   render() {
     return (
-      <div>
+      <div className="container mt-4">
+        <h3 className="mb-3 text-center">Sign In</h3>
         {this.renderError()}
         <form onSubmit={this.onSubmit}>
-          <input 
-            type="text" 
-            name="email" 
-            onChange={this.onEmailChange}
-            autoFocus
-          />
-          <input 
-            type="password" 
-            name="password" 
-            onChange={this.onPasswordChange}
-          />
-          <button disabled={this.isDisabled()}>Login</button>
+          <div className="form-group">
+            <label for="email">Email</label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              onChange={this.onEmailChange}
+              autoFocus
+            />
+          </div>
+          <div className="form-group">
+            <label for="password">Password</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              onChange={this.onPasswordChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary btn-block" disabled={this.isDisabled()}>Login</button>
+          </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   startLogin: (email, password) => dispatch(startLogin(email, password))
 });
 
-export default connect(null, mapDispatchToProps)(LoginPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(LoginPage);
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import validator from 'validator';
 
-import {startSignup} from '../actions/auth';
+import { startSignup } from '../actions/auth';
 
 export class SignupPage extends React.Component {
   constructor(props) {
@@ -10,94 +10,128 @@ export class SignupPage extends React.Component {
 
     this.state = {
       displayName: '',
-      'email': '',
+      email: '',
       password: '',
+      passwordConfirm: '',
       errors: null
-    }
+    };
   }
-  onDisplayNameChange = (e) => {
-    this.setState({displayName: e.target.value})
-  }
-  onEmailChange = (e) => {
-    this.setState({email: e.target.value})
-  }
-  onPasswordChange = (e) => {
-    this.setState({password: e.target.value})
-  }
+  onDisplayNameChange = e => {
+    this.setState({ displayName: e.target.value });
+  };
+  onEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+  onPasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+  onPasswordControlChange = e => {
+    this.setState({ passwordConfirm: e.target.value });
+  };
   renderErrors = () => {
-    const {errors} = this.state;
+    const { errors } = this.state;
     if (errors && errors.length >= 1) {
-      return errors.map((error) => {
-        return <li>{error}</li>
-      })
+      return errors.map(error => {
+        return <li className="list-group-item list-group-item-danger">{error}</li>;
+      });
     }
-  }
-  onSubmit = async (e) => {
-    e.preventDefault()
+  };
+  onSubmit = async e => {
+    e.preventDefault();
 
-    const {displayName, email, password} = this.state;
+    const { displayName, email, password, passwordConfirm } = this.state;
     const errors = [];
 
     if (displayName.length < 6 || displayName.length > 12) {
-      errors.push('Display name must be between 6 and 12 characters.')
+      errors.push('Display name must be between 6 and 12 characters.');
     }
     if (!validator.isEmail(email)) {
-      errors.push('Please enter a valid email.')
+      errors.push('Please enter a valid email.');
     }
     if (password.length < 6 || password.length > 12) {
       errors.push('Password must be between 6 and 12 characters');
-    } 
-    
+    }
+    if (!validator.equals(password, passwordConfirm)) {
+      errors.push('Passwords must match');
+    }
+
     if (errors.length >= 1) {
-      this.setState({errors});
+      this.setState({ errors });
     } else {
       try {
         await this.props.startSignup(displayName, email, password);
         this.props.history.push('/');
       } catch (e) {
         const errors = [];
-        Object.keys(e).forEach((key => {
+        Object.keys(e).forEach(key => {
           errors.push(e[key].message);
-        }));
-        this.setState({errors});
+        });
+        this.setState({ errors });
       }
     }
-  }
+  };
   render() {
     return (
-        <div>
-          <ul className="error-list">
-            {this.renderErrors()}
-          </ul>
-          <form onSubmit={this.onSubmit}>
-            <input 
-              type="text" 
-              name="displayname" 
-              placeholder="Enter a 6-12 character display name" 
+      <div className="container mt-4">
+        <h3 className="mb-3 text-center">Sign Up</h3>
+        <ul className="error-list list-group mb-3">{this.renderErrors()}</ul>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label for="displayname">Display Name:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="displayname"
+              placeholder="Enter a 6-12 character display name"
               onChange={this.onDisplayNameChange}
               autoFocus
             />
-            <input 
-              type="text" 
-              name="email" 
-              placeholder="Enter a valid email" 
+          </div>
+          <div className="form-group">
+            <label for="email">Email:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              placeholder="Enter a valid email"
               onChange={this.onEmailChange}
             />
-            <input 
-              type="password" 
-              name="password" 
-              placeholder="Enter a 6-12 character password" 
+          </div>
+          <div className="form-group">
+            <label for="password">Password:</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              placeholder="Enter a 6-12 character password"
               onChange={this.onPasswordChange}
             />
-            <button>Signup</button>
-          </form>
-        </div>
-    ) 
+          </div>
+          <div className="form-group">
+            <label for="password">Confirm Password:</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              placeholder="Re-enter your password"
+              onChange={this.onPasswordConfirmChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary btn-block">Signup</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  startSignup: (displayName, email, password) => dispatch(startSignup(displayName, email, password))
+const mapDispatchToProps = dispatch => ({
+  startSignup: (displayName, email, password) =>
+    dispatch(startSignup(displayName, email, password))
 });
 
-export default connect(null, mapDispatchToProps)(SignupPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignupPage);
